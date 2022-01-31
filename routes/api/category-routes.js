0313b18c0cde0ba29ee222d0cfb1b8d579c477bc
Matthew Williams <matthewwilliams@matts-mbp.home>
2022-01-31 19:54:53 +0000

    Connected sequelize

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,7 +5,7 @@ const { Category, Product } = require("../../models");
 
 router.get("/", async (req, res) => {
   try {
-    const categoryData = await Location.findAll({
+    const categoryData = await Category.findAll({
       include: [{ model: Product }],
     });
     res.status(200).json(categoryData);
